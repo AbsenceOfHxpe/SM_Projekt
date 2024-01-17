4fe0fe74f77a5fa9b00020a50af8cfa2a81ad058
kziniewicz <kasia.ziniewicz@gmail.com>
2024-01-17 17:31:47 +0100

    google maps api dziala

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
 
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
 }
--- app/src/main/java/com/example/sm_project/Activity/GeolocationActivity.java
+++ app/src/main/java/com/example/sm_project/Activity/GeolocationActivity.java
@@ -15,7 +15,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
+import com.example.sm_project.Fragments.Map_Fragment;
 import com.example.sm_project.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -36,6 +38,8 @@ public class GeolocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geolocation);
+        Fragment fragment = new Map_Fragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
